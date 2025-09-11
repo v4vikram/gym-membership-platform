dff4427e04a618fcc40746dd843ae90d37380774
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-09-11 14:43:04 +0530

    issue solving user id undefined

--- frontend/src/app/(user)/dashboard/membership/page.jsx
+++ frontend/src/app/(user)/dashboard/membership/page.jsx
@@ -2,25 +2,34 @@
 import MembershipInfo from "@/components/MembershipInfo";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useUserMemberStore } from "@/store/useUserMemberStore";
+import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 
 const page = () => {
   const { user, getMe } = useAuthStore();
   const { loading, membership, getUserMembershipById } = useUserMemberStore();
+  const router = useRouter();
 
-useEffect(() => {
-  getMe(); // fetch user first
-}, []);
+  console.log("Rendering page component with user:", user);
 
-useEffect(() => {
-  if (user?._id) {
-    getUserMembershipById(user._id);
-  }
-}, [user?._id]);
+  useEffect(() => {
+    getMe(); // fetch user from backend
+  }, []);
+
+  useEffect(() => {
+    if (user?._id) {
+      getUserMembershipById(user._id);
+    }
+  }, [user?._id]); // run whenever user changes
+
   return (
     <div className="px-6 pt-5 pb-8 text-white relative overflow-hidden min-h-screen">
       <div className="animate-in fade-in slide-in-from-bottom-6 duration-500 ease-out">
-        <MembershipInfo membership={membership} loading={loading} userQrCode={user?.qrCode}/>
+        <MembershipInfo
+          membership={membership}
+          loading={loading}
+          userQrCode={user?.qrCode}
+        />
       </div>
     </div>
   );
