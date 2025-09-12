ce1741cf96255ff4afda12ea073e231c6302f3ef
timewatch-dev <timewatchinfocomwebmail@gmail.com>
2025-09-12 16:53:04 +0530

    auth navbar fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,7 +18,7 @@ export const signup = asyncHandler(async (req, res) => {
 
   res.status(201).json({
     message: "User registered successfully",
-    user: { id: user._id, name: user.name, email: user.email }
+    // user: { id: user._id, name: user.name, email: user.email }
   });
 });
 
--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:8000/api',
-  baseURL: 'https://gym-membership-platform.onrender.com/api',
+  baseURL: 'http://localhost:8000/api',
+  // baseURL: 'https://gym-membership-platform.onrender.com/api',
   withCredentials: true,
 
 });
